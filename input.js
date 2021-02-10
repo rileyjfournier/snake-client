c75ca2bddcb81df3092c1277ccfbf83485dae490
Riley Fournier <rileyjfournier@gmail.com>
2021-02-10 21:42:51 +0000

    created separate input file and updated play.js to accommodate

---
+++ input.js
@@ -0,0 +1,17 @@
+const handleUserInput = function(input) {
+  if(input === '\u0003') {
+    console.log('Exiting game ... ')
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput)
+  stdin.resume();
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-const handleUserInput = function(input) {
-  if(input === '\u0003') {
-    console.log('Exiting game ... ')
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput)
-  stdin.resume();
-  return stdin;
-};
 setupInput();
