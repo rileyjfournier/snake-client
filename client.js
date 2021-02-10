79bd55053c12d45be9ef7da5dc52bb539a659751
Riley Fournier <rileyjfournier@gmail.com>
2021-02-10 20:54:02 +0000

    name shows up on my snake

--- client.js
+++ client.js
@@ -5,11 +5,14 @@ const connect = function() {
     host: '135.23.222.131',
     port: 50542
   });
-  conn.setEncoding('utf8');
-  // ^ interpret incoming data as text
+  conn.setEncoding('utf8');               // interpret data in a text format
+  conn.on('connect', () => {
+    conn.write('Name: RJF');
+    console.log('Successfully connected to game server')
+  })
   conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  // ^ print out the incoming data
+    console.log('Server says: ', data);   // print out the incoming data
+
   });
   return conn;
 };
