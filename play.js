f44ea4606a74117cbe0b89c802a0f313b7eac77a
Riley Fournier <rileyjfournier@gmail.com>
2021-02-10 20:46:24 +0000

    created separate client.js file

--- play.js
+++ play.js
@@ -1,20 +1,3 @@
-const net = require('net');
-// establishes connection with the game server
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-  conn.setEncoding('utf8');
-  // ^ interpret incoming data as text
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  })
-
-  return conn;
-};
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
